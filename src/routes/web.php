0dc4121c646f5db0aaadb3a98e0fb4945c67c057
James Smith <31503183+digitalcrm@users.noreply.github.com>
2019-10-15 13:09:54 +0530

    Update web.php
    Working on this

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,10 +1,11 @@
 <?php
 
-
+/*
 Route::group(['namespace'=>'Digitalcrm\Newsletter\Http\Controllers'], function(){
 
 	Route::get('news','NewsController@index')->name('news');
 	Route::post('news','NewsController@store');
 
 });
+*/
 
